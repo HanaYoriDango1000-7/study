1c2449c279eec3687627685e8715ce93b4aee30a
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-06-02 21:00:33 +0700

    Add project files.

---
+++ oaoaoaoaoaoao/oaoaoaoaoaoao.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+
+
+
+int main()
+{
+	srand(time(0));
+	int inp;
+	int bot = 1 + rand()%5;
+	int count = 0;
+	
+
+	while (count < 3) {
+
+		std::cin >> inp;	
+
+		if (inp == bot) {
+
+			std::cout << "win\n";
+			bot = 1 + rand() % 5;
+			count++;
+		}
+		else {
+
+			std::cout << "lose\n";
+			
+		}
+
+		std::cout << bot << std::endl;
+
+	}
+
+
+
+}
+
