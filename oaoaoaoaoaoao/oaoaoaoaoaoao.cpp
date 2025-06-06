8501da9e99bb76465af94917e1384df5ce8a2b61
Student-2 GameSchool <stud2.gsch124@outlook.com>
2025-06-06 15:50:06 +0700

    test1

--- oaoaoaoaoaoao/oaoaoaoaoaoao.cpp
+++ oaoaoaoaoaoao/oaoaoaoaoaoao.cpp
@@ -1,85 +1,69 @@
 #include <iostream>
 
 
+//void game() {
+//
+//	// Обьявление массив 
+//	std::string x[3]{ "stone","sissors","paper" };
+//
+//	// Создаем переменную для рандома
+//	//int r = x[];
+//	srand(time(0));
+//
+//	int bot = rand() % 3;
+//	std::string temp = x[bot];
+//	std::string player;
+//
+//
+//	// Объявлем начало цикла 
+//	for (int i = 0; i < 5; i++) {
+//
+//		// Игрок вводит свой вариант
+//		std::cin >> player;
+//
+//		// Выводим ход бота 
+//		std::cout << x[bot] << std::endl;
+//
+//		// Сравниваем и выводим win/lose/draw
+//		if (player == x[0] && x[bot] == x[1] ||
+//			player == x[1] && x[bot] == x[2] ||
+//			player == x[2] && x[bot] == x[0]) {
+//
+//			std::cout << "You win\n";
+//
+//		}
+//		else if (player == x[bot]) {
+//
+//			std::cout << "Draw\n";
+//
+//		}
+//		else {
+//			std::cout << "You lose\n";
+//		}
+//	}
 
+int main() {
 
+	int x[5]{ 7, -1, 4, -9, 5 };
 
-int main()
-{
-	// Обьявление массив 
-	std::string x[3]{ "stone","sissors","paper" };
-
-	// Создаем переменную для рандома
-	//int r = x[];
-	srand(time(0));
-
-	int bot = rand()%3;
-	std::string temp = x[bot];
-	std::string player;
-
-
-	// Объявлем начало цикла 
-	for (int i = 0; i < 5; i++) {
-
-		// Игрок вводит свой вариант
-		std::cin >> player;
-
-		// Выводим ход бота 
-		std::cout << x[bot] << std::endl;
-
-		// Сравниваем и выводим win/lose/draw
-		if (player == x[0] && x[bot] == x[1] ||
-			player == x[1] && x[bot] == x[2] ||
-			player == x[2] && x[bot] == x[0]) {
-
-			std::cout << "You win\n";
-
-		}
-		else if (player == x[bot]) {
-
-			std::cout << "Draw\n";
-
-		}
-		else {
-			std::cout << "You lose\n";
-		}
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	int p = -5;
+	p *= -1;
 	
+
+
+	for (int i = 0; i < 5 ; i++) {
+		
+		if (x[i] < 0) {
+
+			x[i] *= -1;
+
+		}
+		
+		std::cout << x[i];
+
+	}
+}
+
 	//std::cout << temp; 
 
 	//char word[4]{ 't','e','s','t' };
@@ -111,5 +95,5 @@ int main()
 
 
 
-}
+
 
