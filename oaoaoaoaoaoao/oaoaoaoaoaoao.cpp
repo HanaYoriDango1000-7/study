c41b0531471ec5c196626f7e90585ad4b555701e
Student-2 GameSchool <stud2.gsch124@outlook.com>
2025-06-06 15:13:01 +0700

    test

--- oaoaoaoaoaoao/oaoaoaoaoaoao.cpp
+++ oaoaoaoaoaoao/oaoaoaoaoaoao.cpp
@@ -6,33 +6,108 @@
 
 int main()
 {
+	// Обьявление массив 
+	std::string x[3]{ "stone","sissors","paper" };
 
+	// Создаем переменную для рандома
+	//int r = x[];
 	srand(time(0));
-	int inp;
-	int bot = 1 + rand()%5;
-	int count = 0;
-	
 
-	while (count < 3) {
+	int bot = rand()%3;
+	std::string temp = x[bot];
+	std::string player;
 
-		std::cin >> inp;	
-		//
-		if (inp == bot) {
 
-			std::cout << "win\n";
-			bot = 1 + rand() % 5;
-			count++;
+	// Объявлем начало цикла 
+	for (int i = 0; i < 5; i++) {
+
+		// Игрок вводит свой вариант
+		std::cin >> player;
+
+		// Выводим ход бота 
+		std::cout << x[bot] << std::endl;
+
+		// Сравниваем и выводим win/lose/draw
+		if (player == x[0] && x[bot] == x[1] ||
+			player == x[1] && x[bot] == x[2] ||
+			player == x[2] && x[bot] == x[0]) {
+
+			std::cout << "You win\n";
+
+		}
+		else if (player == x[bot]) {
+
+			std::cout << "Draw\n";
 
 		}
 		else {
-
-			std::cout << "lose\n";
-			
+			std::cout << "You lose\n";
 		}
-
-		std::cout << bot << std::endl;
-
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	//std::cout << temp; 
+
+	//char word[4]{ 't','e','s','t' };
+
+	//for (int i = 0; i < 4; i++) {
+	//	std::cout << temp[i];
+	//}
+
+	//while (count < 3) {
+
+	//	std::cin >> inp;	
+	//	//
+	//	if (inp == bot) {
+
+	//		std::cout << "win\n";
+	//		bot = 1 + rand() % 5;
+	//		count++;
+
+	//	}
+	//	else {
+
+	//		std::cout << "lose\n";
+	//		
+	//	}
+
+	//	std::cout << bot << std::endl;
+
+	//}
 
 
 
